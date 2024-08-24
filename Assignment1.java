34b81c2fe24e44619d08094936564f6e2731d8c4
abdielo88 <abdiel032713@gmail.com>
2024-08-24 16:55:15 -0400

    Add files via upload

--- Assignment1.java
+++ Assignment1.java
@@ -1,5 +1,17 @@
+/**
+ * This the first assignment for the CEN 4802C course. This class works with the
+ * Fibonacci sequence by using an integer as a position in the sequence to find the
+ * number in that exact position. It consists of two methods, one being the 'fibonacci'
+ * method and the other being the main method which calls on the first one to achieve
+ * the program's purpose.
+ */
 public class Assignment1
 {
+    /**
+     * The main method calls on the 'fibonacci' method and outputs the result using the
+     * integer 'n' which in this case equals to 10; the result outputs to 55.
+     * @param args Used for the command line arguments passed to the program.
+     */
     public static void main(String[] args)
     {
         int n = 10;
@@ -8,6 +20,12 @@ public class Assignment1
         System.out.println("The " + n + "th term of the Fibonacci sequence is " + result + ".");
     }
 
+    /**
+     * This non-void method makes the calculation to find the number in the Fibonacci sequence depending
+     * on the position.
+     * @param n This is the term in the Fibonacci sequence.
+     * @return The number in the nth term.
+     */
     public static int fibonacci(int n)
     {
         if (n <= 1)
